95a2c14e113c8bcd791224e313a592c898709dac
Dariusz Rybicki <darrarski@gmail.com>
2020-07-07 18:24:24 +0200

    Add timer to detail view

--- Demo/Detail.swift
+++ Demo/Detail.swift
@@ -1,19 +1,47 @@
 import ComposableArchitecture
 import SwiftUI
 
-struct DetailState: Equatable {}
+struct DetailState: Equatable {
+    var time: Int = 0
+}
 
-enum DetailAction {}
+enum DetailAction {
+    case startTimer
+    case stopTimer
+    case timerTicked
+}
 
-let detailReducer = Reducer<DetailState, DetailAction, Void>.empty
+let detailReducer = Reducer<DetailState, DetailAction, Void> { state, action, _ in
+    struct TimerId: Hashable {}
+
+    switch action {
+    case .startTimer:
+        return Effect.timer(id: TimerId(), every: 1, tolerance: .zero, on: DispatchQueue.main)
+            .map { _ in DetailAction.timerTicked }
+
+    case .stopTimer:
+        return .cancel(id: TimerId())
+
+    case .timerTicked:
+        state.time += 1
+        return .none
+    }
+
+}
 
 struct DetailView: View {
     let store: Store<DetailState, DetailAction>
 
     var body: some View {
-        VStack(spacing: 16) {
-            Text("Detail").font(.title)
-            Text("Hello, World!")
+        WithViewStore(store) { viewStore in
+            VStack(spacing: 16) {
+                Text("Detail").font(.title)
+                Text("\(viewStore.time)")
+            }.onAppear {
+                viewStore.send(.startTimer)
+            }.onDisappear {
+                viewStore.send(.stopTimer)
+            }
         }
     }
 }
