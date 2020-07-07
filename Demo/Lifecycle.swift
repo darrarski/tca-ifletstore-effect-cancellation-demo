b57cacb659991ab93a49a37d1a5196c784fcfa0d
Dariusz Rybicki <darrarski@gmail.com>
2020-07-07 22:46:27 +0200

    Add Lifecycle helper
    Author: Brandon Williams @mbrandonw
    Source: https://github.com/pointfreeco/swift-composable-architecture/blob/0938b8a17b3be424a3de386dd44c2d7457d831a1/Sources/ComposableArchitecture/SwiftUI/Lifecycle.swift

---
+++ Demo/Lifecycle.swift
@@ -0,0 +1,38 @@
+// author: Brandon Williams @mbrandonw
+// source: https://github.com/pointfreeco/swift-composable-architecture/blob/0938b8a17b3be424a3de386dd44c2d7457d831a1/Sources/ComposableArchitecture/SwiftUI/Lifecycle.swift
+
+import ComposableArchitecture
+
+public enum LifecycleAction<Action> {
+    case onAppear
+    case onDisappear
+    case action(Action)
+}
+
+extension LifecycleAction: Equatable where Action: Equatable {}
+
+extension Reducer {
+    public func lifecycle(
+        onAppear: @escaping (Environment) -> Effect<Action, Never>,
+        onDisappear: @escaping (Environment) -> Effect<Never, Never>
+    ) -> Reducer<State?, LifecycleAction<Action>, Environment> {
+
+        return .init { state, lifecycleAction, environment in
+            switch lifecycleAction {
+            case .onAppear:
+                return onAppear(environment).map(LifecycleAction.action)
+
+            case .onDisappear:
+                return onDisappear(environment).fireAndForget()
+
+            case let .action(action):
+                guard state != nil else {
+                    fatalError()
+                }
+
+                return self.run(&state!, action, environment)
+                    .map(LifecycleAction.action)
+            }
+        }
+    }
+}
